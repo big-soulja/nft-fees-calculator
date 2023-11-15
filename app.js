e4e9dbf0a4cebb841451f388218660e421666073
Petr <sherikhovp@mail.ru>
2023-11-16 00:38:08 +0300

    improved accuracy by checking if amount is undefined

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const settings = {
 const alchemy = new Alchemy(settings);
 
 const contractAddress = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
-//set what creator fees are supposed to be
+//set what royalty fees are supposed to be
 const royalties = 0.025;
 
 const getSalesWithFees = async () => {
   const startTime = new Date();
   let totalWithFees = 0;
+  totalSales = 0;
   let whitelisted = new Set();
   let blacklisted = new Set();
   let pageKey = null; // initialize the pageKey to null
@@ -24,18 +25,30 @@ const getSalesWithFees = async () => {
     const queryParams = {
       contractAddress: contractAddress,
       pageKey: pageKey, // pass the pageKey to the queryParams
+      order: "desc",
     };
     sales = await alchemy.nft.getNftSales(queryParams); // get the sales data for the current page
 
     for (const txn of sales.nftSales) {
-      const totalFee =
-        parseInt(txn.sellerFee.amount) +
-        parseInt(txn.royaltyFee.amount) +
-        parseInt(txn.protocolFee.amount);
+      console.log(txn.sellerAddress);
+      let totalFee =
+        parseInt(txn.sellerFee.amount) + parseInt(txn.royaltyFee.amount);
+      if (typeof txn.protocolFee.amount !== "undefined") {
+        totalFee += parseInt(txn.protocolFee.amount);
+      }
       console.log(totalFee);
+      // console.log(
+      //   "seller: " +
+      //     txn.sellerFee.amount +
+      //     " royalty: " +
+      //     txn.royaltyFee.amount +
+      //     " protocol: " +
+      //     txn.protocolFee.amount
+      // );
+
       if (txn.royaltyFee.amount / totalFee >= royalties) {
-        console.log(txn.sellerAddress);
         totalWithFees++;
+        console.log("sold w/ fees");
         if (!blacklisted.has(txn.sellerAddress)) {
           whitelisted.add(txn.sellerAddress);
         }
@@ -43,14 +56,23 @@ const getSalesWithFees = async () => {
         blacklisted.add(txn.sellerAddress);
       }
     }
+    totalSales += sales.nftSales.length;
     pageKey = sales.pageKey; // update the pageKey to the next page key
   } while (pageKey != null); // repeat until there is no more page
 
-  console.log(totalWithFees / sales.nftSales.length);
-  console.log(totalWithFees);
+  console.log(
+    "There has been " + totalSales + " total sales in this collection"
+  );
+  console.log(
+    "Persentage of people paying proper royalties = " +
+      (totalWithFees / totalSales) * 100 +
+      "%"
+  );
   const endTime = new Date();
   const elapsedTime = endTime - startTime;
-  console.log(blacklisted);
+  console.log(
+    "A total of " + blacklisted.size + " wallets did not pay their fees"
+  );
   console.log(`The program ran for ${elapsedTime} milliseconds.`);
 };
 
